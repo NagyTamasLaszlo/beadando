838f875f8f45ed918d357ab642d5eda02825f0d6
NagyTamasLaszlo <36815336+NagyTamasLaszlo@users.noreply.github.com>
2018-04-29 15:26:28 +0200

    Add files via upload

--- Beadando_8.py
+++ Beadando_8.py
@@ -1,24 +1,19 @@
 first = input("Adja meg az első szót! ")
 second = input("Adja meg a második szót! ")
-els=[]
-mas=[]
-for i in first:
-    els.append(i)
-for i in second:
-    mas.append(i)
 
-k=0
+
+
 kozos=[]
-for i in range(0,len(els)):
-    for j in range(k,len(mas)):
-        if els[i] == mas[j] :
-            kozos.append(mas[j])
-            k+=1
+for i in range(0,len(first)):
+    for j in range(0,len(second)):
+        if first[i] == second[j] :
+            kozos.append(second[j])
+
 vegso=""
 for i in kozos:
     if i not in vegso:
         vegso+=i
-print(kozos)
+
 print(vegso)
 
 
